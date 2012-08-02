d92f8576c6b153eb030948243fb9853d5ace4c3e
Colin Hoffman <hoffma23@cocoa.(none)>
2012-08-01 22:56:34 -0500

    updated the getData to store and return data

--- DataClasses.py
+++ DataClasses.py
@@ -17,17 +17,30 @@ What the written file looks like.  Act like it is a long string of bytes.
 
 """
 
+import sys
+import urllib
+import httplib
+import ConfigParser
+try: import json #python 2.6 included simplejson as json
+except ImportError: import simplejson as json
 
 class Data:
     """
-    A base class for CoMPLICATD data objects
+                header    A base class for CoMPLICATD data objects
     """
     Addama = 1
     S3 = 2
     Local = 3
  
     #should be able to pass the data URI or the synid or both.
-    def __init__(self, synid=None, data_uri=None):
+    def __init__(self, synid=None, data_uri=None, data_loc=None, key=None):
+	if data_loc not in [None,1,2,3]:
+		try:
+			data_loc = {'addama':1,'Addama':1,'S3':2,'s3':2,
+                                    'Local':3, 'local':3}
+		except:
+			data_loc = None
+
         if synid == None and data_uri == None:
 		self.synid = synid
 		self.data_uri = data_uri
@@ -36,15 +49,29 @@ class Data:
 		self.data_uri = None
 	elif data_uri == None:
 		self.data_uri = synid
-		self.data_uri = self.data_uri.strip()
-		self.data_uri = self.data_uri.replace(" ", "%20")
+		self.data_uri = urllib.quote(self.data_uri)
 		self.synid = None
 	else:
 		self.synid = synid
 		self.data_uri = data_uri
-		self.data_uri = self.data_uri.strip()
-		self.data_uri = self.data_uri.replace(" ", "%20")
-	
+		self.data_uri = urllib.quote(self.data_uri)
+
+	try:
+		self.data_uri = self.data_uri.split('https%3A//')[-1]
+		self.data_uri = self.data_uri.split('http%3A//')[-1]
+	except:
+		pass
+
+	if data_loc == None:
+		data_loc = self._discoverLocation()
+	if data_loc == 1:
+		self.Comm = AddamaHandler(key)
+	elif data_loc == 2:
+		self.Comm = S3Handler(key)
+	else:
+		# do local?
+		self.Comm = None
+		pass
 
     def write(self, data_pref):
 	pass
@@ -53,20 +80,41 @@ class Data:
         """
         Take the current data obj, 
         """
+	pass
         
-    def _discoverLocation(self, uri):
+    def _discoverLocation(self):
         """
         From the uri, returns where the data is stored as class variable
         Data.Addama
         Data.S3
         Data.Local
         """
+	return None
 
-    def getData(self, local_cache):
+    def getData(self, local_cache=''):
         """
         Retrieves data from data_uri and stores it in the local cache.
         """
-        
+	if self.Comm is not None and local_cache == '':
+		return self.Comm.readFile(self.data_uri)
+	elif self.Comm is None:
+		return None
+	else:
+		output = self.Comm.readFile(self.data_uri)
+
+	if local_cache == '.':
+		fname = self.data_uri.split('/')[-1]	
+	elif local_cache.count('.'):
+		fname = local_cache
+	elif local_cache.endswith('/'):
+		fname = local_cache+self.data_uri.split('/')[-1]
+	else:
+		fname = local_cache+'/'+self.data_uri.split('/')[-1]
+	
+	f = open(fname, 'w')
+	f.write(output)
+	f.close()
+	return output
 
     def _toFile(self):
         """
@@ -96,15 +144,61 @@ class CommHandler:
         raise Exception("CommHandler is an abstract base class.")
 
 class AddamaHandler(CommHandler):
-    def __init__(self, apicrap):
-        """
-        Read auth settings from file
-        """
-        pass
-
+    def __init__(self, keypath):
+	self.keypath = keypath
+	if not self._handleAuth():
+		print 'Addama Auth failed- invalid apikey file:', keypath
 
     def _handleAuth(self):
-        pass
+	try:
+		self.authFILE = ConfigParser.RawConfigParser()
+		self.authFILE.read(self.keypath)
+		self.Host = self.authFILE.get("Connection", "host")
+		self.ApiKey = self.authFILE.get("Connection", "apikey")
+	except:
+		print "Addama Auth Error:", sys.exc_info()[0]
+		self.authFILE = None
+		self.Host = None
+		self.ApiKey = None
+		return False
+	return True
+
+
+    def readFile(self, uri):
+	if self.Host == None:
+		print "readFile Error: No Addama authorization present"
+		return []
+	
+	try:
+		headers = {"x-addama-apikey": self.ApiKey }
+		params = urllib.urlencode({})
+		conn = httplib.HTTPSConnection(self.Host)
+		uri = uri.lstrip(self.Host)
+		conn.request("GET", uri, params, headers)
+	except:
+		print("Addama readFile Connection/Request Error:",
+                      sys.exc_info()[0])
+		try:
+			conn.close()
+		except:
+			pass
+		return []
+
+	resp = conn.getresponse()
+	if resp.status == 200:
+		output = resp.read()
+		try:
+			retout = json.dumps(json.JSONDecoder().decode(output),
+                                            sort_keys=True, indent=4)
+		except:
+			retout = output
+	else:
+		print("Addama readFile Error:", resp.status, resp.reason)
+		retout = []
+	
+	conn.close()
+	return retout
+		
 
 class S3Handler(CommHandler):
         
@@ -117,6 +211,9 @@ class S3Handler(CommHandler):
 
     def _handleAuth(self):
         pass
+
+    def readFile(self, uri):
+	return []
 
 
 
--- tester.py
+++ tester.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import DataClasses
+import os
+
 
 line = 7
 
@@ -11,15 +13,15 @@ else:
 	print "*FAIL* line:", line, tdatMT.synid, tdatMT.data_uri
 line = line + 7
 
-testData = DataClasses.Data("syn4489", "http://thisguy.com/is/so very cool/yes.html")
-if testData.synid == "syn4489" and testData.data_uri == "http://thisguy.com/is/so%20very%20cool/yes.html":
+testData = DataClasses.Data("syn4489", "thisguy.com/is/so very cool/yes.html")
+if testData.synid == "syn4489" and testData.data_uri == "thisguy.com/is/so%20very%20cool/yes.html":
 	print "correct", testData.synid, testData.data_uri
 else:
 	print "*FAIL* line:", line, testData.synid, testData.data_uri
 line = line + 7
 
-tData = DataClasses.Data("    What does this do?    ")
-if tData.synid == None and tData.data_uri == "What%20does%20this%20do?":
+tData = DataClasses.Data("https://What does this do?")
+if tData.synid == None and tData.data_uri == "What%20does%20this%20do%3F":
 	print "correct", tData.synid, tData.data_uri
 else:
 	print "*FAIL* line:",line , tData.synid, tData.data_uri
@@ -32,7 +34,19 @@ else:
 	print "*FAIL* line:", line, tdat.synid, tdat.data_uri
 line = line + 7
 
-
-
-
+Adata = DataClasses.Data('https://price-external.appspot.com/addama/workspaces/fileshare/Welcome.txt', data_loc=1, key='price-external.apikey')
+AddOut = Adata.getData('.')
+AddFILE = open('Welcome.txt', 'r')
+AddFtxt = AddFILE.read()
+if AddOut == AddFtxt:
+	os.system('rm Welcome.txt')
+	print "correct - file written properly in current directory"
+else:
+	print "*FAIL* file did not write correctly. line:", line+1, AddOut
+	print "Welcome.txt not deleted"
+if AddOut == 'This is the Price Lab External File Share.\n':
+	print "corect -", repr(AddOut)
+else:
+	print "*FAIL* Addama getData (line:", line, ") output is incorrect:\n", AddOut
+line = line + 16
 
